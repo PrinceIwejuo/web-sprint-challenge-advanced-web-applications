9f1887066bf86807ad8b34cbdad8d47d9741cca8
PrinceIwejuo <princeiwejuo@gmail.com>
2021-02-26 19:29:34 -0500

    AdvancedWebAppSprint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
+import PrivateRoute from './components/PrivateRoute';
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from './components/BubblePage';
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Switch>
+        <PrivateRoute exact path='/bubbles' component={BubblePage} />
         <Route exact path="/" component={Login} />
+        </Switch>
       </div>
     </Router>
   );
